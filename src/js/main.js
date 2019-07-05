6440c70b5d166ddd3a88c6ef504af54eca08a364
Damian Rusiecki <damianrusiecki31@gmail.com>
2019-07-05 11:49:44 +0200

    feat: wtf_VII_homework

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,14 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+
+
+
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js')
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
