489c02c64c7f67afc4b3791321aef6d137aeb0a1
Damian Rusiecki <damianrusiecki31@gmail.com>
2019-09-06 16:08:15 +0200

    WTF_XII

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,18 @@ navigationSwitcher.addEventListener('click', (e) => {
     const navigationList = document.querySelector('.navigation__list--js');
     navigationList.classList.toggle('navigation__list--visible');
 });
+
+const person = {
+    name: 'Damian',
+    age: 29
+}
+
+console.log(person);
+
+const jsonPerson = JSON.stringify(person);
+
+console.log(jsonPerson);
+
+// localStorage.setItem('person', jsonPerson);
+
+console.log(localStorage.getItem('person'));
